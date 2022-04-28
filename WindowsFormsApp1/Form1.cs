a6ef3d6eaf3a8a11bad9bae00647e718e1fc4d86
Иван Александрович Дрей <83556084+dreyvania999@users.noreply.github.com>
2022-04-28 10:37:10 +0300

    1

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
@@ -43,25 +36,44 @@ namespace WindowsFormsApp1
 
         static public double GetDeterminant(double[,] matrix)
         {
-           
-            // Если матрица 2*2, то возвращаем определитель по формуле Крамера
-            if (matrix.GetLength(0) == 2)
-                return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
-            int sign = 1;//Знак минора
-            double result = 0;
-            int j = 0;//Номер столбца, по которому раскладывается матрица
-            for (int i = 0; i < matrix.GetLength(0); i++)
+
+            double det=1;
+            int RowAm = sizeMatrix;
+            double[,] MatrixCoef = new double[RowAm, RowAm];
+            double Multi1, Multi2;
+            
+
+            for (int i = 0; i < RowAm; i++)
             {
-                // Если номер столбца и строки одновременно чётные, то знак будет «+», иначе — «-»
-                sign = ((i + 1) % 2 == (j + 1) % 2) ? 1 : -1;
-                result += sign * matrix[i, j] * GetDeterminant(GetMinorMatrix(matrix, i, j));
+                for (int j = 0; j < RowAm; j++)
+                {
+                    MatrixCoef[i, j] = matrix[i, j];
+                }
+
             }
-            return result;
+
+
+            for (int k = 0; k < RowAm; k++)
+            {
+                for (int j = k + 1; j < RowAm; j++)
+                {
+                    Multi1 = MatrixCoef[j, k] / MatrixCoef[k, k];
+                    for (int i = k; i < RowAm; i++)
+                    {
+                        MatrixCoef[j, i] = MatrixCoef[j, i] - Multi1 * MatrixCoef[k, i];
+                    }
+                }
+            }
+            for (int i = 0; i < RowAm; i++)
+            {
+                det *= MatrixCoef[i, i];
+
+            }
+            
+            
+            return det;
         }
 
-        static int minorN, minorM;
-
-        static double[,] minor;
 
         private void button3_Click(object sender, EventArgs e)
         {
@@ -70,31 +82,12 @@ namespace WindowsFormsApp1
             {
                 for (int j = 0; j < sizeMatrix; j++)
                 {
-                    dataGridView1.Rows[i].Cells[j].Value = random.Next(-1,20) ;
+                    dataGridView1.Rows[i].Cells[j].Value = random.Next(-1, 20);
                 }
             }
         }
 
-        static public double[,] GetMinorMatrix(double[,] matrix, int row, int col)
-        {
-            minorN = matrix.GetLength(0) - 1;// уменьшаем размер матрицы
-            minorM = matrix.GetLength(1) - 1;
-            double[,] result = new double[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1];
-            int m = 0, k;
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                if (i == row) continue;
-                k = 0;
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if (j == col) continue;
-                    result[m, k++] = matrix[i, j];
-                }
-                m++;
-            }
-            minor = result;
-            return result;
-        }
+
     }
 
 }
